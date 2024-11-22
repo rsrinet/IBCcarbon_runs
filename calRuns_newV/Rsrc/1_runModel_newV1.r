c953be5c054a3cf965190b32d4715407c927ffeb
Ritika Srinet <88473997+rsrinet@users.noreply.github.com>
2024-11-22 09:13:10 +0200

    Update 1_runModel_newV1.r

--- calRuns_newV/Rsrc/1_runModel_newV1.r
+++ calRuns_newV/Rsrc/1_runModel_newV1.r
@@ -1,5 +1,5 @@
 devtools::source_url("https://raw.githubusercontent.com/rsrinet/IBCcarbon_runs/master/calRuns_newV/Rsrc/settings.r")
-source_url("https://raw.githubusercontent.com/rsrinet/IBCcarbon_runs/refs/heads/master/general/functions.r")
+source_url("https://raw.githubusercontent.com/rsrinet/IBCcarbon_runs/master/general/functions.r")
 
 setX=1
 nSamples <- ceiling(dim(data.all)[1]/nSitesRun)
