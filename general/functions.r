4066c21be3fbbec62dff5ce3ea76d92cad41ff85
Ritika Srinet <88473997+rsrinet@users.noreply.github.com>
2024-12-12 11:04:38 +0200

    Update functions.r

--- general/functions.r
+++ general/functions.r
@@ -1263,7 +1263,7 @@ create_prebas_input.f = function(r_no, clim, data.sample, nYears,
                                 yassoRun = 1,
                                 mortMod = mortMod, 
                                 ECMmod = 1, pCN_alfar= parsCN_alfar, 
-                                alpharVersion = 1,  alpharNcalc = T,
+                                alpharVersion = 3,  alpharNcalc = T,
                                 latitude=latitude, 
                                 p0currClim = p0currClim, 
                                 fT0AvgCurrClim = fT0AvgCurrClim)
