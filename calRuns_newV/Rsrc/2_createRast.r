9db8c6b0f373fd07114a31a322c158fd4cd8a51e
rsrinet <rsrinet@users.noreply.github.com>
2025-03-06 11:16:55 +0200

    update

--- calRuns_newV/Rsrc/2_createRast.r
+++ calRuns_newV/Rsrc/2_createRast.r
@@ -2,7 +2,7 @@
 devtools::source_url("https://raw.githubusercontent.com/rsrinet/IBCcarbon_runs/master/calRuns_newV/Rsrc/settings.r")
 source_url("https://raw.githubusercontent.com/rsrinet/IBCcarbon_runs/master/general/functions.r")
 rcpfile <- rcps
-pathFiles <- paste0("outputDT/forCent",r_no,"/")
+pathFiles <- paste0("outputDT_zb0/forCent",r_no,"/")
 
 # Rasters subdirectory
 raster_subDir <- paste0("rasters/", forCent_folder)
